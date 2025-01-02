1de747ed446d3d476c42cf9191b8d1e4984901fa
Fatima Shahid <160149262+FatimaaShahid@users.noreply.github.com>
2025-01-02 12:58:18 +0500

    Inital Commit

---
+++ 2466_CountWaysToBuildGoodStrings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        mod = 10**9 + 7
+        dp = [0]*(high +1)
+        dp[0] = 1 #empty string's base case
+
+        for length in range(1,high+1) :
+            if length >= zero :
+                dp[length] += dp[length - zero] % mod
+            if length >= one :
+                dp[length] += dp[length - one] % mod
+        return sum(dp[low:high + 1]) % mod
