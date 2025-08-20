ae11b6f8a833f4a71fd4095e0c18deaca79d47f3
Fatima Shahid <160149262+FatimaaShahid@users.noreply.github.com>
2025-08-20 14:38:29 +0500

    Initail Commit

---
+++ AUGUST2025_Streak/1323_Maxmum69Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        num_str = str(num)
+        if "6" in num_str:
+            for i in range(len(num_str)) :
+                if num_str[i] == "6":
+                    return int(num_str[:i] + "9" + num_str[i+1:])
+        return num
+            
+
+
+        
